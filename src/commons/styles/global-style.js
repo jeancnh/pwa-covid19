5dfbf6f79ed88f4fe856f50180b5bacac632eef5
jeancnh <ch2.jean@gmail.com>
2022-05-12 14:15:45 -0300

    versão final

--- src/commons/styles/global-style.js
+++ src/commons/styles/global-style.js
@@ -28,6 +28,18 @@ const globalStyle = createGlobalStyle`
         background-position: center center;
     }
 
+    .mb-2 {
+        margin-bottom: 16px;
+      }
+
+      .pt-2 {
+        padding-top: 16px;
+      }
+      
+      .cursor {
+        cursor: pointer;
+      }
+
 `
 
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,4 @@
+import { Button } from "../../node_modules/@material-ui/core/index"
+
+
+export default Button
